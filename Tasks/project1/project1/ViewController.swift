d4797638673d01529b8a7c62afec3c2816e420ca
Meruert Amambayeva <marylouisekenly@gmail.com>
2019-09-06 21:47:15 +0600

    test

---
+++ Tasks/project1/project1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Meruert Amambayeva on 9/4/19.
+//  Copyright © 2019 Meruert Amambayeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var output: UILabel!
+    
+    
+    
+    
+    
+}
+
